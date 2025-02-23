4864e9772668499acc7e3116674ea854de4660de
Wayne Yeh <thebest77771023@yahoo.com.tw>
2025-02-23 13:43:33 +0800

    add big2

---
+++ big-2/src/test/java/com/example/big2/Big2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.big2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Big2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
